152512cf95c557eebeaf41b1ed6b5be99bf81404
Philip Washington Sorst <philip@sorst.net>
2021-11-03 15:18:35 +0100

    Update to Angular 12

--- test.ts
+++ test.ts
@@ -15,8 +15,10 @@ declare const require: {
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
     BrowserDynamicTestingModule,
-    platformBrowserDynamicTesting()
+    platformBrowserDynamicTesting(),
+    {teardown: {destroyAfterEach: true}},
 );
+
 // Then we find all the tests.
 const context = require.context('./src/', true, /\.spec\.ts$/);
 // And load the modules.
