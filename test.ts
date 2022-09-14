d3a3f09c0e140196dc0f54ddaa49e193158cec76
Philip Washington Sorst <philip@sorst.net>
2022-09-14 13:29:27 +0200

    Update to Angular 14.2

--- test.ts
+++ test.ts
@@ -1,14 +1,14 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/zone';
-import 'zone.js/dist/zone-testing';
+import 'zone.js';
+import 'zone.js/testing';
 import {getTestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
     context(path: string, deep?: boolean, filter?: RegExp): {
-        keys(): string[];
         <T>(id: string): T;
+        keys(): string[];
     };
 };
 
@@ -16,10 +16,9 @@ declare const require: {
 getTestBed().initTestEnvironment(
     BrowserDynamicTestingModule,
     platformBrowserDynamicTesting(),
-    {teardown: {destroyAfterEach: true}},
 );
 
 // Then we find all the tests.
 const context = require.context('./src/', true, /\.spec\.ts$/);
 // And load the modules.
-context.keys().map(context);
+context.keys().forEach(context);
