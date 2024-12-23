08284fb48375265653c876dfb1310cf1cadd880e
Philip Washington Sorst <philip@sorst.net>
2024-12-23 23:28:19 +0100

    refactor: Small optimizations

--- karma.conf.js
+++ karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function (config) {
             suppressAll: true // removes the duplicated traces
         },
         coverageReporter: {
-            dir: require('path').join(__dirname, './coverage/dontdrinkandroot-ngx-lib-scaffold'),
+            dir: require('path').join(__dirname, './coverage'),
             subdir: '.',
             reporters: [
                 {type: 'html'},
